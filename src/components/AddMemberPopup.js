c250b96c1ae9a593d9d368f836cfa290c44b05d3
knguyen93 <khanhnt.tdt@gmail.com>
2020-05-13 18:53:20 -0500

    add backend new url

--- src/components/AddMemberPopup.js
+++ src/components/AddMemberPopup.js
@@ -10,15 +10,15 @@ class FindMemberPopup extends Component {
                 <form>
                     <div className="form-group d-flex">
                         <label className="col-2" for="firstName">First Name <span className="text-danger">*</span></label>
-                        <input type="firstName" id="firstName" className="form-control col-10" placeholder="Enter First Name" />
+                        <input type="firstName" id="firstName" className="form-control col-10" placeholder="Enter First Name"  required/>
                     </div>
                     <div className="form-group d-flex">
                         <label className="col-2" for="lastName">Last Name <span className="text-danger">*</span></label>
-                        <input className="form-control col-10" id="lastName" placeholder="Enter Last Name" />
+                        <input className="form-control col-10" id="lastName" placeholder="Enter Last Name"  required/>
                     </div>
                     <div className="form-group d-flex">
                         <label className="col-2" for="phone">Phone <span className="text-danger">*</span></label>
-                        <input className="form-control col-10" id="phone" placeholder="Enter Phone Number" />
+                        <input className="form-control col-10" id="phone" placeholder="Enter Phone Number"  required/>
                     </div>
                     <div className="form-group d-flex">
                         <label className="col-2" for="street">Street <span className="text-danger">*</span></label>
@@ -26,17 +26,17 @@ class FindMemberPopup extends Component {
                     </div>
                     <div className="form-group d-flex align-items-center">
                         <label className="col-2" for="city">City <span className="text-danger">*</span></label>
-                        <input className="form-control col-4" id="city" placeholder="Enter City" />
+                        <input className="form-control col-4" id="city" placeholder="Enter City"  required/>
                         <label className="col-2 text-right" for="state">State <span className="text-danger">*</span></label>
-                        <input className="form-control col-4" id="state" placeholder="Enter State" />
+                        <input className="form-control col-4" id="state" placeholder="Enter State"  required/>
                     </div>
                     <div className="form-group d-flex mb-4">
                         <label className="col-2" for="zipcode">Zipcode <span className="text-danger">*</span></label>
-                        <input className="form-control col-8" id="zipcode" placeholder="Enter Zipcode" />
+                        <input className="form-control col-8" id="zipcode" placeholder="Enter Zipcode" required/>
                     </div>
                     <hr></hr>
                     <div className="d-flex justify-content-end">
-                        <button className="btn btn-primary mr-2">Submit</button>
+                        <button className="btn btn-primary mr-2" type="submit">Submit</button>
                         <button className="btn btn-secondary">Close</button>
                     </div>
                 </form>
